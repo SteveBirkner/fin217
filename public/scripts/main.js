b41e0acf607cd9b488eaed2172c064500400508f
Steven Birkner <stevebirk36@gmail.com>
2014-05-14 10:46:51 -0400

    Answer to number 1 working laptop page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -48,3 +48,40 @@ function memoryUp(laptop) {
     };
 
 }
+
+//jquery for updating the price  
+$(document).ready(function() {
+   $('input[name="ss"]').on('change', function(){
+        if ($(this).val()=='0') {
+            var cPrice = parseInt(document.getElementById("price").innerHTML);
+            cPrice = cPrice - 200;
+            $("#price").text(cPrice);        
+        } else  {
+           var cPrice = parseInt(document.getElementById("price").innerHTML);
+            cPrice = cPrice + 200;
+            $("#price").text(cPrice);
+        }
+    });
+   $('input[name="ms"]').on('change', function(){
+        if ($(this).val()=='0') {
+            var cPrice = parseInt(document.getElementById("price").innerHTML);
+            cPrice = cPrice - 200;
+            $("#price").text(cPrice);        
+        } else  {
+           var cPrice = parseInt(document.getElementById("price").innerHTML);
+            cPrice = cPrice + 200;
+            $("#price").text(cPrice);
+        }
+    });
+   $('input[name="ds"]').on('change', function(){
+        if ($(this).val()=='0') {
+            var cPrice = parseInt(document.getElementById("price").innerHTML);
+            cPrice = cPrice - 200;
+            $("#price").text(cPrice);        
+        } else  {
+           var cPrice = parseInt(document.getElementById("price").innerHTML);
+            cPrice = cPrice + 200;
+            $("#price").text(cPrice);
+        }
+    });
+});
